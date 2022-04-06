747887ade45d1ff3798cada4dd22a0837e09160c
Gisele <gisele.viedenhelfen@gmail.com>
2022-04-06 17:35:58 -0300

    cria componente Missions

---
+++ src/components/Missions.js
@@ -0,0 +1,12 @@
+import { Component } from 'react/cjs/react.production.min';
+
+const React = require('react');
+
+class Missions extends Component {
+  render() {
+    return (
+      <div data-testid="missions" />
+    );
+  }
+}
+export default Missions;
