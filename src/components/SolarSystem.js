ee550118bad269f04e979273bc17392ac560cb5a
Gisele <gisele.viedenhelfen@gmail.com>
2022-04-06 14:52:42 -0300

    criar componente Solar System

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,12 @@
+import { Component } from 'react/cjs/react.production.min';
+
+const React = require('react');
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system" />
+    );
+  }
+}
+export default SolarSystem;
