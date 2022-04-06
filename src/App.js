1114923cdaf3814762c40404d4d1658283828ffc
Gisele <gisele.viedenhelfen@gmail.com>
2022-04-06 15:19:05 -0300

    colocando o componente Title dentro do SS

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
 
 class App extends React.Component {
   render() {
-    const prop = 'texto';
     return (
       <>
         <Header />
-        <Title headline={ prop } />
         <SolarSystem />
       </>
     );
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,15 @@
 import { Component } from 'react/cjs/react.production.min';
+import Title from './Title';
 
 const React = require('react');
 
 class SolarSystem extends Component {
   render() {
+    const prop = 'Planetas';
     return (
-      <div data-testid="solar-system" />
+      <div data-testid="solar-system">
+        <Title headline={ prop } />
+      </div>
     );
   }
 }
